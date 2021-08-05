e3e0d584052f45ff339970663bd2114f17629873
Radek Wilk <radek.wilk@yahoo.co.uk>
2021-08-05 23:21:14 +0100

    Setting up scss files and creating hero section

---
+++ js/custom_splide.js
@@ -0,0 +1,14 @@
+// Splide is released under the MIT license. © 2020 Naotoshi Fujita
+// https://github.com/Splidejs/splide
+
+new Splide( '.splide', {
+    type: "loop",
+    autoplay: true,
+    interval: 6000,
+    width : '100vw',
+        height: '100vh',
+    arrows: false,
+    //pagination: false,
+    pauseOnHover: false,
+    cover: true,
+  } ).mount();
